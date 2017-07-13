8eb408e4eb67063c2ecae8d65deaa2f44b5f40ce
samedder <samedder@users.noreply.github.com>
2017-07-13 02:16:01 -0700

    Adding remaining commands

--- src/sfcli/commands.py
+++ src/sfcli/commands.py
@@ -32,6 +32,150 @@ class SFCommandLoader(CLICommandsLoader):
                                client_factory=client_create) as super_group:
             with super_group.group('cluster') as group:
                 group.command('health', 'get_cluster_health')
+                group.command('manifest', 'get_cluster_manifest')
+                group.command(
+                    'code-versions',
+                    'get_provisioned_fabric_code_version_info_list'
+                )
+                group.command(
+                    'config-versions',
+                    'get_provisioned_fabric_config_version_info_list'
+                )
+                group.command('upgrade-status', 'get_cluster_upgrade_progress')
+                group.command('recover-system', 'recover_system_partitions')
+                group.command('operation', 'get_fault_operation_list')
+                group.command('operation-cancel', 'cancel_operation')
+
+            with super_group.group('node') as group:
+                group.command('list', 'get_node_info_list')
+                group.command('info', 'get_node_info')
+                group.command('health', 'get_node_health')
+                group.command('load', 'get_node_load_info')
+                group.command('disable', 'disable_node')
+                group.command('enable', 'enable_node')
+                group.command('remove-state', 'remove_node_state')
+                group.command('start', 'start_node')
+                group.command('stop', 'stop_node')
+                group.command('restart', 'restart_node')
+                group.command('transition', 'start_node_transition')
+                group.command(
+                    'transition-status',
+                    'get_node_transition_progress'
+                )
+
+            with super_group.group('application') as group:
+                group.command('type-list', 'get_application_type_info_list')
+                group.command('type', 'get_application_type_info_list_by_name')
+                group.command('provision', 'provision_application_type')
+                group.command('unprovision', 'unprovision_application_type')
+                group.command('delete', 'delete_application')
+                group.command('list', 'get_application_info_list')
+                group.command('info', 'get_application_info')
+                group.command('health', 'get_application_health')
+                group.command('upgrade-status', 'get_application_upgrade')
+                group.command('upgrade-resume', 'resume_application_upgrade')
+                group.command(
+                    'upgrade-rollback',
+                    'rollback_application_upgrade'
+                )
+                group.command(
+                    'deployed-list',
+                    'get_deployed_application_info_list'
+                )
+                group.command('deployed', 'get_deployed_application_info')
+                group.command(
+                    'deployed-health',
+                    'get_deployed_application_health'
+                )
+                group.command('manifest', 'get_application_manifest')
+
+            with super_group.group('service') as group:
+                group.command('type-list', 'get_service_type_info_list')
+                group.command('manifest', 'get_service_manifest')
+                group.command(
+                    'deployed-type-list',
+                    'get_deployed_service_type_info_list'
+                )
+                group.command(
+                    'deployed-type',
+                    'get_deployed_service_type_info_by_name'
+                )
+                group.command('list', 'get_service_info_list')
+                group.command('info', 'get_service_info')
+                group.command('app-name', 'get_application_name_info')
+                group.command('delete', 'delete_service')
+                group.command('description', 'get_service_description')
+                group.command('health', 'get_service_health')
+                group.command('resolve', 'resolve_service')
+                group.command('recover', 'recover_service_partitions')
+                group.command(
+                    'package-list',
+                    'get_deployed_service_package_info_list'
+                    )
+                group.command(
+                    'package-info',
+                    'get_deployed_service_package_info_list_by_name'
+                )
+                group.command(
+                    'package-health',
+                    'get_deployed_service_package_health'
+                )
+                group.command(
+                    'code-package-list',
+                    'get_deployed_code_package_info_list'
+                )
+            
+            with super_group.group('partition') as group:
+                group.command('list', 'get_partition_info_list')
+                group.command('info', 'get_partition_info')
+                group.command('svc-name', 'get_service_name_info')
+                group.command('health', 'get_partition_health')
+                group.command('load', 'get_partition_load_information')
+                group.command('load-reset', 'reset_partition_load')
+                group.command('recover', 'recover_partition')
+                group.command('recover-all', 'recover_all_partitions')
+                group.command('data-loss', 'start_data_loss')
+                group.command('data-loss-status', 'get_data_loss_progress')
+                group.command('quorum-loss', 'start_quorum_loss')
+                group.command('quorum-loss-status', 'get_quorum_loss_progress')
+                group.command('restart', 'start_partition_restart')
+                group.command(
+                    'restart-status',
+                    'get_partition_restart_progress'
+                )
+
+            with super_group.group('replica') as group:
+                group.command('list', 'get_replica_info_list')
+                group.command('info', 'get_replica_info')
+                group.command('health', 'get_replica_health')
+                group.command(
+                    'deployed-list',
+                    'get_deployed_service_replica_info_list'
+                )
+                group.command(
+                    'deployed',
+                    'get_deployed_service_replica_detail_info'
+                )
+                group.command('restart', 'restart_replica')
+                group.command('remove', 'remove_replica')
+
+            with super_group.group('compose') as group:
+                group.command('status', 'get_compose_application_status')
+                group.command('list', 'get_compose_application_status_list')
+                group.command('remove', 'remove_compose_application')
+
+            with super_group.group('chaos') as group:
+                group.command('stop', 'stop_chaos')
+                group.command('report', 'get_chaos_report')
+
+            with super_group.group('store') as group:
+                group.command('stat', 'get_image_store_content')
+                group.command('delete', 'delete_image_store_content')
+                group.command('root-info', 'get_image_store_root_content')
+
+            with super_group.group('is') as group:
+                group.command('command', 'invoke_infrastructure_command')
+                group.command('query', 'invoke_infrastructure_query')
 
         # Custom commands
 
@@ -72,7 +216,7 @@ class SFCommandLoader(CLICommandsLoader):
             with super_group.group('service') as group:
                 group.command('create', 'create')
                 group.command('update', 'update')
-                group.command('package-upload', 'package_upload')
+                group.command('package-deploy', 'package_upload')
 
         return OrderedDict(self.command_table)
 
