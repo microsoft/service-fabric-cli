1327f7ca938e2931d4f0040a5a7005fbc6d51fd5
samedder <samedder@users.noreply.github.com>
2017-09-16 16:52:27 -0700

    Adding classifier

--- src/setup.py
+++ src/setup.py
@@ -32,6 +32,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
     keywords='servicefabric azure',
