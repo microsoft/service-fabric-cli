0098c62d288790780cb4a98495fae5ba05aa802d
Mohammad Tanviruzzaman <motanv@microsoft.com>
2018-01-23 07:48:47 -0800

    src/sfctl/custom_chaos.py(64): [E1121start] Too many positional arguments for constructor call

--- src/sfctl/custom_chaos.py
+++ src/sfctl/custom_chaos.py
@@ -61,7 +61,8 @@ def start(client, time_to_run="4294967295", max_cluster_stabilization=60, #pylin
 
     target_filter = parse_chaos_target_filter(chaos_target_filter)
 
-    chaos_params = ChaosParameters(time_to_run, max_cluster_stabilization, #pylint: disable=too-many-arguments,too-many-locals,
+    #pylint: disable-msg=too-many-arguments
+    chaos_params = ChaosParameters(time_to_run, max_cluster_stabilization,
                                    max_concurrent_faults,
                                    not disable_move_replica_faults,
                                    wait_time_between_faults,
@@ -69,5 +70,6 @@ def start(client, time_to_run="4294967295", max_cluster_stabilization=60, #pylin
                                    health_policy,
                                    context,
                                    target_filter)
+    #pylint: enable-msg=too-many-arguments  
 
     client.start_chaos(chaos_params, timeout)
