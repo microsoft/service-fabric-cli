91bb8c4db4ae2693909e777199c9760a8b20d048
Mohammad Tanviruzzaman <motanv@microsoft.com>
2018-01-23 07:31:24 -0800

    Fix travis ci errors

--- src/sfctl/custom_chaos.py
+++ src/sfctl/custom_chaos.py
@@ -61,7 +61,7 @@ def start(client, time_to_run="4294967295", max_cluster_stabilization=60, #pylin
 
     target_filter = parse_chaos_target_filter(chaos_target_filter)
 
-    chaos_params = ChaosParameters(time_to_run, max_cluster_stabilization, #pylint: disable=too-many-arguments
+    chaos_params = ChaosParameters(time_to_run, max_cluster_stabilization, #pylint: disable=too-many-arguments,too-many-locals,
                                    max_concurrent_faults,
                                    not disable_move_replica_faults,
                                    wait_time_between_faults,
