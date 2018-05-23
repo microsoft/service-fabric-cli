58ece92488ba04b6c2d498e9d08ffdb9e6e4b1dc
Christina-Kang <bikang@microsoft.com>
2018-05-22 18:15:36 -0700

    Made sfctl help into example format

--- src/sfctl/helps/chaos.py
+++ src/sfctl/helps/chaos.py
@@ -197,20 +197,13 @@ helps['chaos schedule set'] = """
         If the version provided matches the version on the server, then the
         Chaos Schedule is updated and the version of the Chaos Schedule on the
         server is incremented up by one and wraps back to 0 after 2,147,483,647.
-
-
         If Chaos is running when this call is made, the call will fail.
+    examples: 
+        - name: The following command sets a schedule (assuming the current schedule has version 0) that starts on 2016-01-01 and expires on 2038-01-01 that runs Chaos 24 hours of the day, 7 days a week. Chaos will be scheduled on the cluster for that time.
+          text: sfctl chaos schedule set --version 0 --start-date-utc "2016-01-01T00:00:00.000Z" --expiry-date-utc "2038-01-01T00:00:00.000Z" --chaos-parameters-dictionary [{\\\"Key\\\":\\\"adhoc\\\",\\\"Value\\\":{\\\"MaxConcurrentFaults\\\":3,\\\"EnableMoveReplicaFaults\\\":true,\\\"ChaosTargetFilter\\\":{\\\"NodeTypeInclusionList\\\":[\\\"N0010Ref\\\",\\\"N0020Ref\\\",\\\"N0030Ref\\\",\\\"N0040Ref\\\",\\\"N0050Ref\\\"]},\\\"MaxClusterStabilizationTimeoutInSeconds\\\":60,\\\"WaitTimeBetweenIterationsInSeconds\\\":15,\\\"WaitTimeBetweenFaultsInSeconds\\\":30,\\\"TimeToRunInSeconds\\\":\\\"600\\\",\\\"Context\\\":{\\\"Map\\\":{\\\"test\\\":\\\"value\\\"}},\\\"ClusterHealthPolicy\\\":{\\\"MaxPercentUnhealthyNodes\\\":0,\\\"ConsiderWarningAsError\\\":true,\\\"MaxPercentUnhealthyApplications\\\":0}}}] --jobs [{\\\"ChaosParameters\\\":\\\"adhoc\\\",\\\"Days\\\":{\\\"Sunday\\\":true,\\\"Monday\\\":true,\\\"Tuesday\\\":true,\\\"Wednesday\\\":true,\\\"Thursday\\\":true,\\\"Friday\\\":true,\\\"Saturday\\\":true},\\\"Times\\\":[{\\\"StartTime\\\":{\\\"Hour\\\":0,\\\"Minute\\\":0},\\\"EndTime\\\":{\\\"Hour\\\":23,\\\"Minute\\\":59}}]}]
 
 
-        Example
-
-        The following command sets a schedule (assuming the current schedule has version 0) that starts on 2016-01-01 and expires on 2038-01-01 that runs Chaos 24 hours of the day, 7 days a week.
-
-
-        sfctl chaos schedule set --version 0 --start-date-utc "2016-01-01T00:00:00.000Z" --expiry-date-utc "2038-01-01T00:00:00.000Z" --chaos-parameters-dictionary [{\\\"Key\\\":\\\"adhoc\\\",\\\"Value\\\":{\\\"MaxConcurrentFaults\\\":3,\\\"EnableMoveReplicaFaults\\\":true,\\\"ChaosTargetFilter\\\":{\\\"NodeTypeInclusionList\\\":[\\\"N0010Ref\\\",\\\"N0020Ref\\\",\\\"N0030Ref\\\",\\\"N0040Ref\\\",\\\"N0050Ref\\\"]},\\\"MaxClusterStabilizationTimeoutInSeconds\\\":60,\\\"WaitTimeBetweenIterationsInSeconds\\\":15,\\\"WaitTimeBetweenFaultsInSeconds\\\":30,\\\"TimeToRunInSeconds\\\":\\\"600\\\",\\\"Context\\\":{\\\"Map\\\":{\\\"test\\\":\\\"value\\\"}},\\\"ClusterHealthPolicy\\\":{\\\"MaxPercentUnhealthyNodes\\\":0,\\\"ConsiderWarningAsError\\\":true,\\\"MaxPercentUnhealthyApplications\\\":0}}}] --jobs [{\\\"ChaosParameters\\\":\\\"adhoc\\\",\\\"Days\\\":{\\\"Sunday\\\":true,\\\"Monday\\\":true,\\\"Tuesday\\\":true,\\\"Wednesday\\\":true,\\\"Thursday\\\":true,\\\"Friday\\\":true,\\\"Saturday\\\":true},\\\"Times\\\":[{\\\"StartTime\\\":{\\\"Hour\\\":0,\\\"Minute\\\":0},\\\"EndTime\\\":{\\\"Hour\\\":23,\\\"Minute\\\":59}}]}]
-
-
-        Chaos will now be scheduled on the cluster.
+        
 
     parameters:
         - name: --version
