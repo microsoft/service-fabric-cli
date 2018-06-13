d53c4a70a73ba1a17522e9c38d07c4aa1b439734
Anant Shankar <ashank@microsoft.com>
2018-06-13 13:29:14 +0530

    Adding Yaml generation in the init command

--- src/sfctl/custom_resource.py
+++ src/sfctl/custom_resource.py
@@ -7,6 +7,16 @@
 """Commands related to managing Service Fabric Mesh resources"""
 
 from knack.util import CLIError
+from pathlib import Path
+import os
+import yaml
+import shutil
+from collections import OrderedDict
+
+def ordered_dict_representer(self, value):
+    return self.represent_mapping('tag:yaml.org,2002:map', value.items())
+
+yaml.add_representer(OrderedDict, ordered_dict_representer)
 
 def get_yaml_content(file_path):
     """ Loads the yaml content for the given file path
@@ -135,20 +145,119 @@ def create_deployment_resource(client, application_resource_name, file_path, tim
     print("Content:\n")
     print(content)
 
-def init_application_resource(client, application_resource_name, file_path, timeout=60): 
-    from azure.servicefabric.models.application_resource_description import ApplicationResourceDescription
-    content = get_yaml_content(file_path)
-    print("Content:\n")
-    print(content)
-
 def init_network_resource(client, application_resource_name, file_path, timeout=60): 
     from azure.servicefabric.models.application_resource_description import ApplicationResourceDescription
     content = get_yaml_content(file_path)
     print("Content:\n")
     print(content)
 
-def init_volume_resource(client, application_resource_name, file_path, timeout=60): 
-    from azure.servicefabric.models.application_resource_description import ApplicationResourceDescription
-    content = get_yaml_content(file_path)
-    print("Content:\n")
-    print(content)
+def init_volume_resource(client, volume_name, volume_provider='sfAzureFile', timeout=60): 
+    """ Initialize the volume context
+    :param volume_name: Volume resource name
+    :param volume_provider: Provider of the volume resource
+    """
+    file_path = os.path.join(os.getcwd(), "servicefabric", "volume.yaml")
+
+    #if volume yaml doesn't exists, create    
+    if not Path(file_path).exists():
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file_data = OrderedDict([
+            ('volume', OrderedDict([
+            ('schemaVersion','0.0.1'),
+            ('name', volume_name),
+            ('description' , volume_name + ' description.'),
+            ('sharingType', 'shared'),
+            ('provider', volume_provider),
+            ('params', OrderedDict([
+                ('shareName', 'helloWorldShare'),
+                ('accountName', 'testAccount'),
+                ('accountKey', 'xyz')
+            ]))
+            ]))            
+            ]
+        )
+        with open(file_path, 'w') as file_path:
+            yaml.dump(file_data, file_path, default_flow_style=False)
+
+
+def init_application_resource(client, application_resource_name, add_service_name=None, delete_service_name=None, containerostype='Windows', networkreference='network', timeout=60): 
+    """ Initialize the application context
+    :param application_resource_name: Application resource name.
+    :param add_service_name: Add a new service to the context with the given name.
+    :param delete_service_name: Delete the service from the context with the given name.
+    """    
+    file_path = os.path.join(os.getcwd(), "servicefabric", "application.yaml")
+
+    #if application yaml doesn't exists, create    
+    if not Path(file_path).exists():
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file_data = OrderedDict([
+            ('application', OrderedDict([
+            ('schemaVersion','0.0.1'),
+            ('name', application_resource_name),
+            ('description' , application_resource_name + ' description.')]))
+            ]
+        )
+        with open(file_path, 'w') as file_path:
+            yaml.dump(file_data, file_path, default_flow_style=False)
+        
+    # check if any service can be added or deleted
+    if add_service_name != None:
+        add_service_file = os.path.join(os.getcwd(), "serviceresource", add_service_name, "service.yaml")
+        if Path(add_service_file).exists:
+            CLIError(add_service_name + " service manifest already present.")
+        
+        directory = os.path.dirname(add_service_file)            
+        if os.path.exists(directory):
+            CLIError(directory + " directory already present.")
+        os.makedirs(directory)            
+
+        # create service manifest
+        file_data = OrderedDict([
+            ('service', OrderedDict([
+                ('name', add_service_name),
+                ('description', add_service_name + ' description.'),
+                ('osType', containerostype),
+                ('codePackages', OrderedDict([
+                    ('name', add_service_name),
+                    ('image', add_service_name),
+                    ('endpoints', OrderedDict([
+                        ('name', add_service_name + 'Listener'),
+                        ('port', 20003)
+                    ])),
+                    ('resources', OrderedDict([
+                        ('requests', OrderedDict([
+                            ('cpu', '0.5'),
+                            ('memoryInGB', '1')    
+                        ]))                        
+                    ]))                    
+                ])),
+                ('replicaCount', '1'),
+                ('networkRefs', OrderedDict([
+                    ('name', add_service_name + networkreference)
+                ]))
+            ])
+            )
+        ])
+       
+        with open(add_service_file, 'w') as add_service_file:
+            yaml.dump(file_data, add_service_file, default_flow_style=False)        
+       
+            
+    # check if any service can be deleted
+    if delete_service_name != None:
+        delete_service_file = os.path.join(os.getcwd(), "serviceresource", delete_service_name, "service.yaml")
+
+        directory = os.path.dirname(delete_service_file)            
+        if not os.path.exists(directory):
+            CLIError(directory + " directory is not present.")
+        # delete service manifest and dir            
+        shutil.rmtree(directory)
+
+
