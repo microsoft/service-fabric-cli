5ea3e9d4ae6c4ddc8f10f1a9ea3a3e8251b1b307
Christina Kang - MSFT <Christina-Kang@users.noreply.github.com>
2018-12-10 11:31:17 -0800

    Add batched telemetry (#166)
    Improve upon #152 to make telemetry send batched telemetry in order to improve performance.
    
    Telemetry will write to a file and hold on to telemetry to send in chunks. Current value is 50 items per batch. There are limited retries, so that if there is something such as no internet connection, we retry intermittently

--- src/sfctl/send_telemetry_background.py
+++ src/sfctl/send_telemetry_background.py
@@ -7,71 +7,96 @@
 """A script which sends telemetry in the background as a new process.
 This process should end itself in SINGLE_UPLOAD_TIMEOUT seconds for single telemetry calls."""
 
-import sys
-from uuid import uuid4
+import os
+import json
 from multiprocessing import Process
+import portalocker
 from applicationinsights import TelemetryClient
-from sfctl.config import get_cli_version_from_pkg
+from sfctl.telemetry import TELEMETRY_FILE_PATH
+from sfctl.state import set_telemetry_send_retry_count
 
 INSTRUMENTATION = '482faeea-c22b-4c75-a1af-5bfe79f36cb7'
-SINGLE_UPLOAD_TIMEOUT = 15
+SINGLE_UPLOAD_TIMEOUT = 30
 
-
-def send_telemetry_best_attempt(command_name, telemetry_data):
+def read_telemetry_entries():
     """
-    Sends telemetry without retry. On failure, telemetry data is lost, and no records are kept.
+    Read and return the telemetry entries.
 
-    :param command_name: (str) the command_name being called, without parameters.
-        For example, 'node show'
-    :param telemetry_data: (dict) Dict containing the following data, where all inputs are str
-        {'success': call_success,
-         'operating_system': platform,
-         'python_version': python_version,
-         'operation_id': operation_id,
-         'error_msg': error_msg,
-         'sfctl_version': version}
+    :return: List[(str, dict)] returns a list of tuples representing the input
+        to the track_event command for the application insights telemetry client.
+        Each tuple contains the following:
+            (str) the command_name being called, without parameters. For example, 'node show'
+            (dict) See return from get_telemetry_input_as_dict()
+    """
+
+    return_tuples = []
+
+    # Mode r starts at the beginning of the file
+    with portalocker.Lock(TELEMETRY_FILE_PATH, timeout=1, fail_when_locked=True, mode='r') as telemetry_file:  # pylint: disable=line-too-long
+        all_lines = telemetry_file.readlines()
+
+        # Parse the lines. The lines have format {0}, {1}
+        for line in all_lines:
+            if line.strip():  # ignore any empty lines
+                index_of_first_comma = line.find(',')
+                command_name = line[:index_of_first_comma]
+                json_string = line[index_of_first_comma+1:]
+                telemetry_dict = json.loads(json_string)
+                return_tuples.append((command_name, telemetry_dict))
+
+    return return_tuples
+
+
+def send_telemetry_best_attempt():
+    """
+    Sends telemetry data from file TELEMETRY_FILE_PATH. The data is preserved if the process
+    is terminated before telemetry is sent, since removal of the TELEMETRY_FILE_PATH is called
+    only once that is completed.
+
+    There is a possibility of duplicate data, since if the telemetry data is sent out, and the
+    process is terminated before the file is deleted, we may end up resending.
+    This is acceptable since it will be rare, and since we have unique IDs for each telemetry
+    event.
+
+    There is a possibility that the process is terminated after telemetry send and file delete,
+    but before we can resent the telemetry send retry counter. This case will also be rare.
+    It will also take care of itself over time, since we will keep filling up new data, and
+    eventually it will send. We may lose some data, but that can be fixed in future changes.
+
+    We are also not currently setting time on the telemetry. We are counting the time as the time
+    sent. There is an entry to track the time, but the default sorting will not work with time
+    necessarily. We accept this right now, since there is not much we do with time data.
 
     :return: None
     """
 
-    telemetry_client = TelemetryClient(INSTRUMENTATION)
-    # This information is also repeated in telemetry_data for ease of search.
-    # This may be moved from one area to another
-    telemetry_client.context.application.ver = get_cli_version_from_pkg()
+    telemetry_tuples = read_telemetry_entries()
 
-    telemetry_client.track_event(command_name, telemetry_data)
+    telemetry_client = TelemetryClient(INSTRUMENTATION)
+
+    for tup in telemetry_tuples:
+        telemetry_client.track_event(tup[0], tup[1])
 
     # This will never end if there is no internet connection, for example.
     telemetry_client.flush()
+
+    # After send has completed, delete the file
+    try:
+        os.remove(TELEMETRY_FILE_PATH)
+    except:  # pylint: disable=bare-except
+        pass
+
+    # After send has completed, clear the telemetry retry counter
+    set_telemetry_send_retry_count(0)
 
 # pylint: disable=invalid-name
 if __name__ == '__main__':
 
     try:
-
-        command = sys.argv[1]
-        call_success = sys.argv[2]
-        platform = sys.argv[3]
-        python_version = sys.argv[4]
-        error_msg = sys.argv[5]
-
-        operation_id = str(uuid4())
-
-        # operation_id is used to ID one instance of a user calling a command.
-
-        telemetry_data_input = {'success': call_success,
-                                'operating_system': platform,
-                                'python_version': python_version,
-                                'operation_id': operation_id,
-                                'error_msg': error_msg,
-                                'sfctl_version': get_cli_version_from_pkg()}
-
         # Using a process because we can kill that on a timer. We cannot kill threads the same way.
         telemetry_name = 'sfctl_telemetry'
         send_current_telemetry_process = Process(target=send_telemetry_best_attempt,
-                                                 name=telemetry_name,
-                                                 args=(command, telemetry_data_input))
-        send_current_telemetry_process.name = telemetry_name
+                                                 name=telemetry_name)
 
         send_current_telemetry_process.start()
         send_current_telemetry_process.join(SINGLE_UPLOAD_TIMEOUT)
