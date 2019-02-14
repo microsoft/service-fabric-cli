eefba5b067809cb7bf5641e89f8e763b63286323
Christina-Kang <Christina-Kang@users.noreply.github.com>
2019-02-14 14:12:40 -0800

    update knack

--- src/setup.py
+++ src/setup.py
@@ -44,7 +44,7 @@ setup(
         'sfctl.tests'
     ],
     install_requires=[
-        'knack==0.5.1',
+        'knack==0.5.2',
         'msrest>=0.4.26',
         'msrestazure',
         'requests',
