054937ec464231f62eef9561a43ab536058e7238
Ivan Nedic <ivan@microsoft.com>
2019-10-28 21:31:20 +0100

    Fix in params

--- src/sfctl/params.py
+++ src/sfctl/params.py
@@ -184,6 +184,9 @@ def custom_arguments(self, _):  # pylint: disable=too-many-statements
         arg_context.argument('upgrade_domain_delta_unhealthy_nodes', type=int)
         arg_context.argument('app_health_map', type=json_encoded)
 
+    with ArgumentsContext(self, 'node add-configuration-parameter-overrides') as arg_context:
+        arg_context.argument('config_parameter_override_list', type=json_encoded)
+
     with ArgumentsContext(self, 'sa-cluster config-upgrade') as arg_context:
         arg_context.argument('unhealthy_applications', type=int)
         arg_context.argument('unhealthy_nodes', type=int)
