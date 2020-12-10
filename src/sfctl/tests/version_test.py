783066dfd1c51603f0160323bf2ae14913645f6b
jeffj6123 <jeffrey.jarry@gmail.com>
2020-12-10 15:16:23 -0800

    removed error

--- src/sfctl/tests/version_test.py
+++ src/sfctl/tests/version_test.py
@@ -16,7 +16,7 @@ class VersionTests(unittest.TestCase):
 
         note: this will require changing the sfctl_version on releases
         """
-        sfctl_version = '11.s0.0'
+        sfctl_version = '11.0.0'
 
         pipe = Popen('sfctl --version', shell=True, stdout=PIPE, stderr=PIPE)
         # returned_string and err are returned as bytes
