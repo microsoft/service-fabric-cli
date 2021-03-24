57e8790de267785a0bd7b9a8714e694f0f7a6fde
jeffj6123 <jeffrey.jarry@gmail.com>
2021-03-24 12:58:49 -0700

    8.0 temp sdk (#233)
    * updated.
    
    * removed accident line.

--- customSDK/servicefabric/aio/operations_async/__init__.py
+++ customSDK/servicefabric/aio/operations_async/__init__.py
@@ -9,16 +9,16 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from ._mesh_secret_operations import MeshSecretOperations
-from ._mesh_secret_value_operations import MeshSecretValueOperations
-from ._mesh_volume_operations import MeshVolumeOperations
-from ._mesh_network_operations import MeshNetworkOperations
-from ._mesh_application_operations import MeshApplicationOperations
-from ._mesh_service_operations import MeshServiceOperations
-from ._mesh_code_package_operations import MeshCodePackageOperations
-from ._mesh_service_replica_operations import MeshServiceReplicaOperations
-from ._mesh_gateway_operations import MeshGatewayOperations
-from ._service_fabric_client_ap_is_operations import ServiceFabricClientAPIsOperationsMixin
+from ._mesh_secret_operations_async import MeshSecretOperations
+from ._mesh_secret_value_operations_async import MeshSecretValueOperations
+from ._mesh_volume_operations_async import MeshVolumeOperations
+from ._mesh_network_operations_async import MeshNetworkOperations
+from ._mesh_application_operations_async import MeshApplicationOperations
+from ._mesh_service_operations_async import MeshServiceOperations
+from ._mesh_code_package_operations_async import MeshCodePackageOperations
+from ._mesh_service_replica_operations_async import MeshServiceReplicaOperations
+from ._mesh_gateway_operations_async import MeshGatewayOperations
+from ._service_fabric_client_ap_is_operations_async import ServiceFabricClientAPIsOperationsMixin
 
 __all__ = [
     'MeshSecretOperations',
