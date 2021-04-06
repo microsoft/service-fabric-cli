480742bd7dc08277499a85f01d2d529ea648de9c
jeffj6123 <jeffrey.jarry@gmail.com>
2021-04-06 10:45:57 -0700

    Update request_generation_test.py
    change the way tags are passed in.

--- src/sfctl/tests/request_generation_test.py
+++ src/sfctl/tests/request_generation_test.py
@@ -473,15 +473,16 @@ class ServiceFabricRequestTests(ScenarioTest):
              '"ParameterValue": "False"}]'),
             validate_list_of_objects)
         node_tags = path.join(sample_path_base, 'sample_node_tags.txt').replace('/', '//').replace('\\', '\\\\')
+        tags = '"[\\"tagA\\", \\"tagB\\", \\"tagC\\"]"'
         self.validate_command(  # add-node-tags
-            'sfctl node add-node-tags --node-name=nodeName --node-tags=["tagA", "tagB", "tagC"]',
+            'sfctl node add-node-tags --node-name=nodeName --node-tags=' + tags,
             'POST',
             '/Nodes/nodeName/$/AddNodeTags',
             ['api-version=8.0'],
             ('{"NodeTagsList":["tagA", "tagB", "tagC"]}'),
             validate_list_of_objects)
         self.validate_command(  # remove-node-tags
-            'sfctl node remove-node-tags --node-name=nodeName --node-tags=["tagA", "tagB", "tagC"]',
+            'sfctl node remove-node-tags --node-name=nodeName --node-tags=' + tags,
             'POST',
             '/Nodes/nodeName/$/RemoveNodeTags',
             ['api-version=8.0'],
