adc79062e22f9825cf747ac2f445b7e055f6fa24
jeffj6123 <jeffrey.jarry@gmail.com>
2022-04-12 16:48:40 -0700

    starting to grab the right config.

--- src/sfctl/apiclient.py
+++ src/sfctl/apiclient.py
@@ -8,10 +8,16 @@
 
 from knack.util import CLIError
 from azure.servicefabric import ServiceFabricClientAPIs
+from azure.core.credentials import TokenCredential
 
 from sfctl.auth import (ClientCertAuthentication, AdalAuthentication)
 from sfctl.config import (security_type, ca_cert_info, cert_info,
                           client_endpoint, no_verify_setting)
+
+class dummmy_protocol(TokenCredential):
+    def __init__(self):
+        self
+
 
 def create(_):
     """Create a client for Service Fabric APIs."""
@@ -30,19 +36,12 @@ def create(_):
     if security_type() == 'aad':
         auth = AdalAuthentication(no_verify)
     else:
-        cert = cert_info()
         ca_cert = ca_cert_info()
-        auth = ClientCertAuthentication(cert, ca_cert, no_verify)
+        if ca_cert is not None:
+            no_verify = ca_cert
+    
+    dummy_credential = dummmy_protocol()
 
-    client = ServiceFabricClientAPIs(auth, base_url=endpoint)
-
-    # client.config.retry_policy has type msrest.pipeline.ClientRetryPolicy
-    client.config.retry_policy.total = False
-    client.config.retry_policy.policy.total = False
-
-    # msrest defines ClientRetryPolicy in pipline.py.
-    # ClientRetryPolicy.__init__ defines values for status_forcelist
-    # which is passed to urllib3.util.retry.Retry
-    client.config.retry_policy.policy.status_forcelist = None
+    client = ServiceFabricClientAPIs(dummy_credential, base_url=endpoint, retry_total=0, connection_verify=no_verify, connection_cert=cert_info())
 
     return client
--- src/sfctl/auth.py
+++ src/sfctl/auth.py
@@ -70,3 +70,35 @@ class AdalAuthentication(Authentication):
         header = "{} {}".format("Bearer", new_token['accessToken'])
         session.headers['Authorization'] = header
         return session
+
+class AdalAuthentication2():
+    """Azure Active Directory authentication for Service Fabric clusters"""
+
+    # def __init__(self, no_verify=False):
+    #     self.no_verify = no_verify
+
+    def get_header(self):
+        """Create requests session with AAD auth headers
+
+        :rtype: requests.Session.
+        """
+
+        from sfctl.config import (aad_metadata, aad_cache)
+
+        if session:
+            session = super(AdalAuthentication, self).signed_session(session)
+        else:
+            session = super(AdalAuthentication, self).signed_session()
+
+        if self.no_verify:
+            session.verify = False
+
+        authority_uri, cluster_id, client_id = aad_metadata()
+        existing_token, existing_cache = aad_cache()
+        context = adal.AuthenticationContext(authority_uri,
+                                             cache=existing_cache)
+        new_token = context.acquire_token(cluster_id,
+                                          existing_token['userId'], client_id)
+        header = "{} {}".format("Bearer", new_token['accessToken'])
+        # session.headers['Authorization'] = header
+        return header
