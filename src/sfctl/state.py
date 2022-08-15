667a1fb2d84108ae61f5bcca6b8c2d70aa859114
jeffj6123 <jeffrey.jarry@gmail.com>
2022-08-15 15:28:27 -0700

    revert state.py changes.

--- src/sfctl/state.py
+++ src/sfctl/state.py
@@ -9,6 +9,7 @@
 import os
 from datetime import datetime
 from knack.config import CLIConfig
+from pkg_resources import get_distribution
 
 # knack CLIConfig has all the functionality needed to keep track of state, so we are using that
 # here to prevent code duplication. We are using CLIConfig to create a file called 'state' to
@@ -36,7 +37,6 @@ def get_state_path():
 
 def get_state_value(name, fallback=None):
     """Gets a state entry by name.
-
     In the case where the state entry name is not found, will use fallback value."""
 
     cli_config = CLIConfig(SF_CLI_STATE_DIR, SF_CLI_NAME, 'state')
@@ -47,7 +47,6 @@ def get_state_value(name, fallback=None):
 def set_state_value(name, value):
     """
     Set a state entry with a specified a value.
-
     :param name: (str) name of the state
     :param value: (str) value of the state
     :return: None
@@ -59,10 +58,8 @@ def set_state_value(name, value):
 
 def get_cluster_version_check_time():
     """Get the time that the cluster version was last checked.
-
     Return as a datetime.datetime object which represents a UTC time, even though timezone is not
     explicitly stated.
-
     Return None if the value does not exist in state"""
 
     datetime_str = get_state_value('datetime', None)
@@ -76,10 +73,8 @@ def get_cluster_version_check_time():
 def set_cluster_version_check_time(custom_time=None):
     """Set the time that the cluster version was last checked.
     Time values are given in UTC, but no timezone information is set.
-
     If custom_time is not provided, set as the current time in UTC.
     If custom_time is provided, set a custom_time.
-
     :param custom_time: For testing only. Expects UTC, but without time zone information.
     :type custom_time: datetime.datetime object"""
 
@@ -92,7 +87,6 @@ def set_cluster_version_check_time(custom_time=None):
 def get_telemetry_send_retry_count():
     """
     Get the number of send telemetry attempts have failed consecutively.
-
     :return: int representing number of retries.
              Return None if the value does not exist in state
     """
@@ -107,7 +101,6 @@ def get_telemetry_send_retry_count():
 
 def set_telemetry_send_retry_count(retry_count):
     """Set the number of send telemetry attempts have failed consecutively.
-
     :param retry_count: Number of retries for sending telemetry
     :type retry_count: int"""
 
@@ -118,7 +111,6 @@ def increment_telemetry_send_retry_count():  # pylint: disable=invalid-name
     """
     Gets the current telemetry send retry count and increments the value by 1.
     If no value is currently set, set the value to 1.
-
     :return: (int) The new incremented value
     """
 
@@ -136,10 +128,5 @@ def get_sfctl_version():
     Get the version of the sfctl. For example, 6.0.0
     :return: str
     """
-    try:
-        from importlib.metadata import version
-        return version('sfctl')
-    except: # pylint: disable=W0702
-        from pkg_resources import get_distribution
-        pkg = get_distribution("sfctl")
-        return pkg.version
+    pkg = get_distribution("sfctl")
+    return pkg.version
